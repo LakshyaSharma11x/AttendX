7b83900ba574a2fe77101b8f9c49762aa1ea3f62
Lakshya Sharma <lakshyasharma11052002@gmail.com>
2024-06-28 22:57:07 +0530

    add splash screen animation

--- app/src/main/java/om/example/attendx/splash_screen.java
+++ app/src/main/java/om/example/attendx/splash_screen.java
@@ -3,6 +3,9 @@ package om.example.attendx;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,19 +15,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class splash_screen extends AppCompatActivity {
 
+    TextView textAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        textAnimation = findViewById(R.id.textAnimation);
+
+        Animation size = AnimationUtils.loadAnimation(this,R.anim.size);
+        textAnimation.setAnimation(size);
 
         Intent intent = new Intent(this,MainActivity.class);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                textAnimation.setAnimation(size);
                 startActivity(intent);
                 finish();
             }
-        },4000);
+        },6000);
     }
 }
